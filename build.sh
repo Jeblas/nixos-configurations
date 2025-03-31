f40c2f7afba90fb7a3a630348f06e5635f0af41d
John Blasco <>
2025-03-31 01:02:24 -0400

    restore hashed password

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-nixos-rebuild switch --flake .#tigris
+sudo nixos-rebuild switch --flake .#tigris
 # sudo nix --extra-experimental-features nix-command --extra-experimental-features flakes build .#nixosConfigurations.tigris.config.system.build.toplevel -L -j22
 # sudo ./result/bin/switch-to-configuration switch
--- users/jeblas/base.nix
+++ users/jeblas/base.nix
@@ -1,13 +1,13 @@
 { config, lib, pkgs, ... }:
 {
   users = {
-    mutableUsers = false;
+    mutableUsers = true;
     users.jeblas = {
       isNormalUser = true;
       description = "John";
       home = "/home/jeblas";
       uid = 1000;
-      # hashedPassword = "$y$j9T$JDnjfZlgeJlPTsuVD91rS0$vEMav2JL1VYdR6qDkOyFwO0RTAZZH7g/nnDXm9FuauC"; # use mkPassword to generate
+      hashedPassword = "$y$j9T$JDnjfZlgeJlPTsuVD91rS0$vEMav2JL1VYdR6qDkOyFwO0RTAZZH7g/nnDXm9FuauC"; # use mkPassword to generate
       extraGroups = [ "wheel" ] ++ lib.optionals config.networking.networkmanager.enable [ "networkmanager" ];
 
       openssh.authorizedKeys.keys = [
