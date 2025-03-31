434a01682abfc458aba90a7f344e7dbf813ca80b
John Blasco <>
2025-03-31 00:50:03 -0400

    remove password and corrected switch commands

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
-sudo nix --extra-experimental-features nix-command --extra-experimental-features flakes build .#nixosConfigurations.tigris.config.system.build.toplevel -L -j22
-sudo ./result/bin/switch-to-configuration switch
+nixos-rebuild switch --flake .#tigris
+# sudo nix --extra-experimental-features nix-command --extra-experimental-features flakes build .#nixosConfigurations.tigris.config.system.build.toplevel -L -j22
+# sudo ./result/bin/switch-to-configuration switch
--- users/jeblas/base.nix
+++ users/jeblas/base.nix
@@ -7,7 +7,7 @@
       description = "John";
       home = "/home/jeblas";
       uid = 1000;
-      hashedPassword = "$y$j9T$JDnjfZlgeJlPTsuVD91rS0$vEMav2JL1VYdR6qDkOyFwO0RTAZZH7g/nnDXm9FuauC"; # use mkPassword to generate
+      # hashedPassword = "$y$j9T$JDnjfZlgeJlPTsuVD91rS0$vEMav2JL1VYdR6qDkOyFwO0RTAZZH7g/nnDXm9FuauC"; # use mkPassword to generate
       extraGroups = [ "wheel" ] ++ lib.optionals config.networking.networkmanager.enable [ "networkmanager" ];
 
       openssh.authorizedKeys.keys = [
